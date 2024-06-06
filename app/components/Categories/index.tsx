ea3a224f3f2f318a0349f3dfa199caca2bceabdd
Sey-Uki <abakarova.diana99@gmail.com>
2024-06-06 21:09:58 +0300

    add categories

--- app/components/Categories/index.tsx
+++ app/components/Categories/index.tsx
@@ -3,22 +3,28 @@ import React, { useState } from "react";
 import styled from "styled-components/native";
 import { useThemeColor } from "@/hooks/useThemeColor";
 import { Cappuccino } from "@/app/icons/Cappuccino";
+import { Coldbrew } from "@/app/icons/Coldbrew";
+import { Espresso } from "@/app/icons/Espresso";
 
 const categories = [
   { name: "Cappuccino", icon: <Cappuccino /> },
-  { name: "Cold Bre", icon: <Cappuccino /> },
-  { name: "Espresso", icon: <Cappuccino /> },
+  { name: "Cold Brew", icon: <Coldbrew /> },
+  { name: "Espresso", icon: <Espresso /> },
 ];
 
 export function Categories() {
-  const selectedCategories = useState();
+  const [activeCategoryIndex, setActiveCategoryIndex] = useState(0);
 
   const color = useThemeColor({}, "text");
 
   return (
     <View>
-      {categories.map((item) => (
-        <Container>
+      {categories.map((item, index) => (
+        <Container
+          key={item.name}
+          onPress={() => setActiveCategoryIndex(index)}
+          active={index === activeCategoryIndex}
+        >
           {item.icon}
           <Text style={{ color }}>{item.name}</Text>
         </Container>
@@ -38,9 +44,9 @@ const Text = styled.Text`
   font-weight: 800;
 `;
 
-const Container = styled.View`
+const Container = styled.Pressable<{ active: boolean }>`
   height: 30px;
-  background-color: #846046;
+  ${({ active }) => active && "background-color: #846046"};
 
   align-items: center;
   flex-direction: row;
